723024468da332c7135a69029a18e6e489eb0221
Ahmad <41236167+ahmadinator@users.noreply.github.com>
2022-12-22 04:15:31 +0400

    Add operate function

---
+++ main.js
@@ -0,0 +1,8 @@
+let operate = function(operator, a, b) {
+    switch (operator) {
+        case '+': return a + b
+        case '-': return a - b
+        case '*': return a * b
+        case '/': return a / b
+    }
+}
